2a382a9d8273d03cfeb637734eef13cfe6619083
kuwin <54054246+kuwin@users.noreply.github.com>
2023-03-07 19:39:52 +0700

    Update stratum_mining.sh

--- stratum_mining.sh
+++ stratum_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe7vmd4sj9xsuus8rmt3q -r stratum+tls://dero.rabidmining.com:10300 -p stratum;
     sleep 5;
 done
